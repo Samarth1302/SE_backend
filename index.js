e5a3370a76532e34ec231562e4a388bd5a8cf024
Samarth Chauhan <samarth.chauhan13022002@gmail.com>
2023-10-21 18:39:49 +0530

    changed-auth (error still there)

--- index.js
+++ index.js
@@ -2,24 +2,45 @@ const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const auth = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  const path = req.originalUrl || req.url;
+  if (path === "/signup" || path === "/login") {
+    next();
+  } else {
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Access denied. No token provided" });
+    }
+    try {
+      const user = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = user;
+      next();
+    } catch (error) {
+      console.error("JWT Verification Error:", error);
+      return res.status(400).json({ error: "Invalid token" });
+    }
+  }
+};
+
 const startServer = async () => {
   const app = express();
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: ({ req, res }) => ({
-      claims: auth(req, res, () => {}),
+      claims: auth(req, res),
     }),
   });
 
   await server.start();
   server.applyMiddleware({ app });
-  app.use("/api", bodyParser.json());
 
   const PORT = process.env.PORT || 3000;
 
@@ -35,21 +56,6 @@ const startServer = async () => {
       });
     })
     .catch((err) => console.error(`Error: ${err.message}`));
-};
-
-const auth = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({ error: "Access denied. No token provided" });
-  }
-  try {
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = user;
-    next();
-  } catch (error) {
-    console.error("JWT Verification Error:", error);
-    res.status(400).json({ error: "Invalid token" });
-  }
 };
 
 startServer();
