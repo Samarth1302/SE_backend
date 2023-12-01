adac5dc7a9cde82146239a112b244c292f123910
Samarth Chauhan <samarth.chauhan13022002@gmail.com>
2023-12-01 11:02:56 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000/",
     credentials: true,
   },
 });
